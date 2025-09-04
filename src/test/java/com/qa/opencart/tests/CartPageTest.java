bff8a6d81da004c59cc6db35f178571a3538281a
Naveen AutomationLabs <naveenanimation20@gmail.com>
2025-09-04 07:05:01 +0530

    added cart test

---
+++ src/test/java/com/qa/opencart/tests/CartPageTest.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class CartPageTest {
+	
+	
+	@Test
+	public void cartTest() {
+		System.out.println("cart testing");
+	}
+	
+
+}
